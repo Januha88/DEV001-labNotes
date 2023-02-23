b6cff6934fb16d2b14c4c78b6d045fc1b1e5cfb8
Januha88 <januharyg@gmail.com>
2023-02-23 15:45:06 -0300

    actualizando boilerPlate

--- src/App.jsx
+++ src/App.jsx
@@ -17,11 +17,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx </code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
